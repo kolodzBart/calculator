e74ac130df527a0dc67c6acaa3b42c91702d866d
Bartłomiej Kołodziejczyk <kolodz.bart@gmail.com>
2019-11-06 18:52:49 +0100

    creating first look

--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -4,9 +4,19 @@ namespace calculator
 {
     class Program
     {
+        double firstnum;
+        double secondnum;
+        double answer;
+        string ops;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Console Calculator\r");
+            Console.WriteLine("-------------------\r");
+
+            Console.WriteLine("Enter first number\r");
+            Console.ReadKey();
+
         }
     }
 }
