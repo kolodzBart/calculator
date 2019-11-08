01c4006ec696343376736e397b8d489733965f11
Bartłomiej Kołodziejczyk <kolodz.bart@gmail.com>
2019-11-08 22:14:47 +0100

    rules for operator

--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -12,6 +12,7 @@ namespace calculator
             double answer;
             string ops;
             string message;
+            bool opstype;
 
             Console.WriteLine("Console Calculator\r");
             Console.WriteLine("-------------------\r");
@@ -19,8 +20,17 @@ namespace calculator
             Console.WriteLine("Enter first number\r");
             firstnum = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Select an operator: (+,-,*,/)\r");
-            ops = Console.ReadLine();
+            // Console.WriteLine("Select an operator: (+,-,*,/)\r");
+            // ops = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Select an operator: (+,-,*,/)\r");
+                ops = Console.ReadLine();
+
+                opstype = Equals(ops, "+") || Equals(ops, "-") || Equals(ops, "/") || Equals(ops, "*");
+            }
+            while (opstype == false);
 
             Console.WriteLine("Enter second number\r");
             secondnum = double.Parse(Console.ReadLine()); 
@@ -53,6 +63,7 @@ namespace calculator
                     Console.Write(answer);
                 }
             }
+            Console.ReadKey();
         }
     
     }
