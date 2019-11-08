9c17f7c1ca3f4e75fe414e2c88c243849b084bf9
Bartłomiej Kołodziejczyk <kolodz.bart@gmail.com>
2019-11-08 21:38:42 +0100

    add four rules

--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -4,19 +4,56 @@ namespace calculator
 {
     class Program
     {
-        double firstnum;
-        double secondnum;
-        double answer;
-        string ops;
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            double firstnum;
+            double secondnum;
+            double answer;
+            string ops;
+            string message;
+
             Console.WriteLine("Console Calculator\r");
             Console.WriteLine("-------------------\r");
 
             Console.WriteLine("Enter first number\r");
-            Console.ReadKey();
+            firstnum = double.Parse(Console.ReadLine());
 
+            Console.WriteLine("Select an operator: (+,-,*,/)\r");
+            ops = Console.ReadLine();
+
+            Console.WriteLine("Enter second number\r");
+            secondnum = double.Parse(Console.ReadLine()); 
+
+            if (ops == "+")
+            {
+                answer = firstnum + secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "-")
+            {
+                answer = firstnum - secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "*")
+            {
+                answer = firstnum * secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "/")
+            {
+                if (secondnum == 0)
+                {
+                    message = "You cannot divide by 0";
+                    Console.Write(message);
+                }
+                else
+                {
+                    answer = firstnum / secondnum;
+                    Console.Write(answer);
+                }
+            }
         }
+    
     }
 }
